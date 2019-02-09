df84d1f2cd8c99ed768c9fbeef8f65cb6aba5ba4
Felipe Ximenes <ximenesfel@gmail.com>
2019-02-09 18:59:20 -0200

    Plotting is working but the direction is not good

---
+++ myDrone.py
@@ -0,0 +1,31 @@
+from udacidrone import Drone
+
+import numpy as np
+import visdom
+
+
+class MyDrone(Drone):
+
+    def __init__(self, connection):
+        super().__init__(connection)
+
+        # default opens up to http://localhost:8097
+        self.v = visdom.Visdom()
+        assert self.v.check_connection()
+
+        # Plot NE
+        ne = np.array([self.local_position[0], self.local_position[1]]).reshape(1, -1)
+        self.ne_plot = self.v.scatter(ne, opts=dict(
+            title="Local position (north, east)",
+            xlabel='North',
+            ylabel='East'
+        ))
+
+        # Plot D
+        d = np.array([self.local_position[2]])
+        self.t = 0
+        self.d_plot = self.v.line(d, X=np.array([self.t]), opts=dict(
+            title="Altitude (meters)",
+            xlabel='Timestep',
+            ylabel='Up'
+        ))
